12abdd9cfad6acf410b999621b0bbe32c87bc523
FedericoMorrillo <federicomorrillo98@gmail.com>
2023-11-07 11:01:57 +0100

    fix: importo finale  sconti in 2 decimali

--- js/main.js
+++ js/main.js
@@ -4,26 +4,26 @@
 const prezzoAlKm = 0.21;
 
 // definiamo le due variabili per chiedere all' utente informazioni convertendo la stringa in valore numerico
-const età = parseInt( prompt ('inserisci la tua età'));
-const kmDaPercorrere = parseInt( prompt ('quanti Km devi percorrere?'));
+const età = Number(prompt('inserisci la tua età'));
+const kmDaPercorrere = Number(prompt('quanti Km devi percorrere?'));
 
 // formula per il calcolo del prezzo e impostiamo l importo in 2 decimali
 let prezzo = (kmDaPercorrere * prezzoAlKm).toFixed(2);
 
 // messaggio del console.log
-let msg= "Il prezzo del biglietto è di: " + prezzo + "€";
+let msg = "Il prezzo del biglietto è di: " + prezzo + "€";
 
 // condizioni per inserire gli sconti
-if (età < 18){
-     prezzo = prezzo - (prezzo / 100 * 20);
+if (età < 18) {
+     prezzo = (prezzo - (prezzo / 100 * 20)).toFixed(2);
      msg = "il prezzo del biglietto è di: " + prezzo + "€ con uno sconto del 20% sul prezzo totale";
 }
 
-else if(età >= 65){
-     prezzo = prezzo - (prezzo / 100 * 40) ;
+else if (età >= 65) {
+     prezzo = (prezzo - (prezzo / 100 * 40)).toFixed(2);
      msg = "il prezzo del biglietto è di: " + prezzo + "€ con uno scondo del 40% sul prezzo totale";
 }
 
-  alert(msg);  
+alert(msg);
 
 
